cbbeda86c308aa163b2ea01464295d1254cdd62f
Alejandro Alfaro Sanchez <alejandro@alejandroalsa.es>
2022-12-05 18:24:40 +0100

    test 03
    Signed-off-by: Alejandro Alfaro Sanchez <alejandro@alejandroalsa.es>

--- Practica-03/bash/scripts/deploy_web.sh
+++ Practica-03/bash/scripts/deploy_web.sh
@@ -2,7 +2,10 @@
 
 clear
 set -x
-source variables.sh
+
+LAMP_DB=alejandro
+LAMP_USER=alejandro
+LAMP_PASS=alejandro
 
 # Movimiento a la carpeta de tmp.
     cd /tmp
@@ -16,7 +19,7 @@ source variables.sh
     rm -fr phpMyAdmin
 
 # Ejecución del Script de sql para la creación de la base de datos. 
-    mysql -u root < /tmp/iaw-practica-lamp/db/database.sql
+    mysql -u root < iaw-practica-lamp/db/database.sql
 
 # Creación de un usuario
     mysql -u root <<< "CREATE USER IF NOT EXISTS '$LAMP_USER'@'%'"
@@ -27,26 +30,26 @@ source variables.sh
 # Asignación de privilegios al usuario
     mysql -u root <<< "GRANT ALL PRIVILEGES ON $LAMP_DB.* TO '$LAMP_USER'@'%';"
 
+# Modificación del campo lamp_db para la sustitución del nuestro
+    sed -i "s/'lamp_db'/$LAMP_DB/" /var/www/html/config.php  
+
+# Modificación del campo lamp_user para la sustitución del nuestro
+    sed -i "s/'lamp_user'/$LAMP_USER/" /var/www/html/config.php  
+
+# Modificación del campo lamp_password para la sustitución del nuestro 
+    sed -i "s/'lamp_password'/$LAMP_PASS/" /var/www/html/config.php
+
 # Movimiento de todos los archivos a /var/www/html
     mv /tmp/iaw-practica-lamp/src/* /var/www/html
 
-# Modificación del campo lamp_db para la sustitución del nuestro
-    sed -i "s/'lamp_db'/$LAMP_DB/" /var/www/html/config.php
-
-# Modificación del campo lamp_user para la sustitución del nuestro
-    sed -i "s/'lamp_user'/$LAMP_USER/" /var/www/html/config.php
-
-# Modificación del campo lamp_password para la sustitución del nuestro 
-    sed -i "s/'lamp_password'/$LAMP_PASS/" /var/www/html/config.php  
-
 # Asignación de usario y grupo a todos los archivos de /var/www/html
-    chown www-data:www-data /var/www/html -R 
+    chown www-data:www-data /var/www/html -R
 
 # Eliminación de archivo por defecto de Apache2.4
     rm -f /var/www/html/index.html
 
 # Reinicio del servidor web Apache2.4
     systemctl restart apache2
-    
+
 # En el caso de que este establecida un acontraseña para root
     # mysql -u root -pPASSWORD < /tmp/iaw-practica-lamp/db/database.sql
